e7c6e5b1ceadb1717274c3dfc934bec7e4df89de
fraserk <kimfraser@gmail.com>
2014-06-16 22:36:15 -0500

    add photos

--- app/views/static/about.blade.php
+++ app/views/static/about.blade.php
@@ -5,7 +5,7 @@
 
 			<div class="row">
         <div class="large-12 columns text-left">
-        <img src="img/profile.JPG" height="100px" width="100px" align="">
+        <img src="img/profile.JPG"  height="100px" width="106px" align="">
         <img src="img/profile2.JPG" height="100px" width="100px" align="">
         <img src="img/profile3.JPG" height="100px" width="100px" align="">
         <img src="img/profile4.JPG" height="100px" width="100px" align="">
